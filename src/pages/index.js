8356da881f546c8d2603b6e7d6e8bb31983397c3
DOOCOM <doocom-design-01@DOOCOMdeiMac.local>
2018-09-28 15:40:25 +0800

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>你好</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>这是我的第一个网站</p>
+    <p>哈哈</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
